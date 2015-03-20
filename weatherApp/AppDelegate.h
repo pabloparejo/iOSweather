12e988be78e6860e92e7e5d34825044a420828b3
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-03-20 18:55:14 +0100

    Initial Commit

---
+++ weatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weatherApp
+//
+//  Created by Pablo Parejo Camacho on 20/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weatherAppTests/weatherAppTests.m
@@ -0,0 +1,40 @@
+//
+//  weatherAppTests.m
+//  weatherAppTests
+//
+//  Created by Pablo Parejo Camacho on 20/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface weatherAppTests : XCTestCase
+
+@end
+
+@implementation weatherAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
