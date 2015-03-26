9c0374b44097e73168afa69e7fdac3b826e5ac4e
pabloparejo <parejo@iMac-de-parejo.local>
2015-03-27 00:57:07 +0100

    adding custom cell class

---
+++ PARForecastTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  weatherApp
+//
+//  Created by parejo on 26/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ PARForecastTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  weatherApp
+//
+//  Created by parejo on 26/3/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
